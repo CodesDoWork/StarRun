cc3805a0b1c932341bc97367b14ae5e7a235d5e5
Justin Konratt <justin.konratt@clausohm.de>
2023-06-23 23:00:09 +0200

    refactor(start-screen): remove unnecessary transition options

--- app/src/main/java/de/host/mobsys/starrun/MainActivity.java
+++ app/src/main/java/de/host/mobsys/starrun/MainActivity.java
@@ -5,8 +5,6 @@ import static de.host.mobsys.starrun.control.PreferenceInfo.HIGHSCORE;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.core.app.ActivityOptionsCompat;
-
 import de.host.mobsys.starrun.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
@@ -19,11 +17,6 @@ public class MainActivity extends BaseActivity {
         setContentView(binding.getRoot());
 
         binding.highscore.setText(getString(R.string.highscore, storage.get(HIGHSCORE)));
-        binding.play.setOnClickListener(v -> {
-            ActivityOptionsCompat options
-                = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
-            Intent intent = new Intent(this, GameActivity.class);
-            startActivity(intent, options.toBundle());
-        });
+        binding.play.setOnClickListener(v -> startActivity(new Intent(this, GameActivity.class)));
     }
 }
